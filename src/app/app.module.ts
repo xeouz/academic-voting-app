517c34289049f218a8cabd9689e6906d88ed2f04
XeouzDev0 <agencyxhq@gmail.com>
2023-06-07 18:07:18 +0530

    Author UI Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app'
 import { provideDatabase, getDatabase } from '@angular/fire/database'
+import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon'
 import { MatDialogModule } from '@angular/material/dialog'
@@ -47,6 +48,7 @@ import { ClassDataComponent } from './components/class-data/class-data.component
     ClassDataComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
